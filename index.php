829be53505ca881500eb56fce24a3254a87852fc
SuperJanis <40403047+SuperJanis@users.noreply.github.com>
2018-07-02 09:52:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ $rawBody  = file_get_contents('php://input');
 # Get as an object
 $data = json_decode($rawBody);
 
-$data['queryResult']['parameters']['fulfillmentMessages']['text']['text'] = "Cinq sur cinq";
+//$data['queryResult']['parameters']['fulfillmentMessages']['text']['text'] = "Cinq sur cinq";
+$response['fulfuillmentText'] = "Cinq sur cinq";
 
-echo json_encode($data);
+echo json_encode($response);
 
 ?>
