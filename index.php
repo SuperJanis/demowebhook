7acc96579291effef2696bb9e117129e57127ae8
SuperJanis <40403047+SuperJanis@users.noreply.github.com>
2018-07-02 12:06:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,12 @@
 $rawBody  = file_get_contents('php://input');
 # Get as an object
 $data = json_decode($rawBody);
-$request = '';
-
-
-//$response['fulfillmentText'] = $request;
-echo $data->queryResult->queryText;
 
 $request = $data->queryResult->queryText;
 
+//$response['fulfillmentText'] = $request;
+
 echo $request;
-
-//var_dump($data['queryResult']['queryText']);
-//var_dump($data);
-
 
 //echo json_encode($response);
 ?>
