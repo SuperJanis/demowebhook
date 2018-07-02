01331a53bfebe6d294be746b4946e3a390762925
SuperJanis <40403047+SuperJanis@users.noreply.github.com>
2018-07-02 10:50:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ $rawBody  = file_get_contents('php://input');
 # Get as an object
 $data = json_decode($rawBody);
 
-//$data['queryResult']['parameters']['fulfillmentMessages']['text']['text'] = "Cinq sur cinq";
-$response['fulfillmentText'] = "Cinq sur cinq";
+$request = $data['queryResult']['queryText'];
+$response['fulfillmentText'] = "<speak><prosody rate="x-fast"><say-as interpret-as="characters"> $request </say-as></prosody></speak>";
+
 
 echo json_encode($response);
 
